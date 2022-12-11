cda20991f02444557c28e026b985bc6a524a3162
Daniel Mulcahy <ZetaetaDaniel@gmail.com>
2022-12-11 13:54:21 +0000

    beta client

---
+++ src/BigWord.tsx
@@ -0,0 +1,14 @@
+import  React  from "react";
+
+class BigWords extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = { word: "" };
+  }
+
+  render() {
+    return <div>
+      <p>number</p>
+    </div>;
+  }
+}
