392615e836b8167585271cb3947a370c427c5946
Daniel Mulcahy <ZetaetaDaniel@gmail.com>
2022-12-22 18:46:24 +0000

    WSE

--- src/WordSourceEditor.tsx
+++ src/WordSourceEditor.tsx
@@ -1,5 +1,35 @@
-import { Form } from "react-bootstrap";
+import { useRef } from "react";
+import { Button, Form } from "react-bootstrap";
 
-export function WordSourceEditor({ sources }: { sources: { file: string }[] }) {
-  return <Form></Form>;
+export function WordSourceEditor({
+  sources,
+}: {
+  sources: { weight: number; file: string }[];
+}) {
+  const filesRef = useRef(Array(sources.length));
+  function submit(event: any) {
+    filesRef.current.map((field, i) => {
+      const value = field.value;
+    });
+  }
+  return (
+    <Form onSubmit={submit} className="text-center">
+      {sources.map((source, i) => {
+        return (
+          <Form.Group>
+            <Form.Label>{source.file}</Form.Label>
+            <Form.Control
+              type="number"
+              defaultValue={source.weight}
+              name={source.file}
+              ref={(el: any) => (filesRef.current[i] = el)}
+            ></Form.Control>
+          </Form.Group>
+        );
+      })}
+      <Button type="submit" value="Submit">
+        Submit
+      </Button>
+    </Form>
+  );
 }
